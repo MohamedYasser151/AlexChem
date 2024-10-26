4b976e6754a4bfb8b1654482c81943d9ae3383ff
mohamed yasser <mohamedyasser12346262@gmail.com>
2024-10-26 13:42:37 +0300

    update project6

--- frontend/src/component/data2.js
+++ frontend/src/component/data2.js
@@ -8,7 +8,7 @@ import { NavLink } from "react-router-dom";
 
 const fetchProductPrices = async () => {
     try {
-        const response = await fetch("http://localhost:8083/products");
+        const response = await fetch("https://alexchem-server.vercel.app/products");
         if (!response.ok) {
             throw new Error("Failed to fetch product data");
         }
